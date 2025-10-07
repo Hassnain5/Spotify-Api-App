e9f37d30f3b6f3031292ec6665b077db2119df63
Syed Noor ul Hassnain <134180882+Hassnain5@users.noreply.github.com>
2025-10-07 12:11:00 +0500

    Update Consts.dart

--- lib/Consts/Consts.dart
+++ lib/Consts/Consts.dart
@@ -1,5 +1,5 @@
 
 class Consts{
-  static const String clientId = '0d20ac36044643749be91cb770822494';
-  static const String clientSecret = 'b95ef8acea25456f9cd99628afbe8c71';
+  static const String clientId = '';
+  static const String clientSecret = '';
 }
